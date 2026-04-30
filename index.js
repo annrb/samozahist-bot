427372323b2113ee38e521415a96da84e9a7f499
annrb <reichkol@icloud.com>
2026-04-30 19:39:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -406,30 +406,10 @@ app.post("/", async (req, res) => {
 
     // Замовлення / вільне повідомлення
   if (isOrderMessage && !msg.photo && !msg.video && !msg.document) {
-    await sendMessage(chatId, "Я зайшов у блок замовлення");
-    const parts = text.split(",").map(x => x.trim());
-
-if (parts.length >= 5) {
-  const order = {
-    name: parts[0],
-    phone: parts[1],
-    city: parts[2],
-    delivery: parts[3],
-    product: parts.slice(4).join(", ")
-  };
-
-  const cleanPhone = order.phone.replace(/\D/g, "");
-  const validPhone =
-    /^0\d{9}$/.test(cleanPhone) ||
-    /^380\d{9}$/.test(cleanPhone);
-
-  if (!validPhone) {
-    await sendMessage(
-      chatId,
-      "❌ Невірний номер телефону.\n\nПриклад: 0971234567 або +380971234567"
-    );
-    return;
-  }
+  await sendMessage(chatId, "Я зайшов у блок замовлення");
+  await sendMessage(chatId, "ТЕСТ 2");
+  return;
+}
 
   pendingOrders.set(chatId, {
     ...order,
