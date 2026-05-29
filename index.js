73df926d4e1ad4cf454f22295dfc9a5a3fb10281
annrb <reichkol@icloud.com>
2026-05-29 22:29:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -507,7 +507,19 @@ if (data === "clearcart") {
 
   await sendMessage(
     adminChatId,
-    "🗑 Кошик очищено"
+    "🗑 Кошик очищено.\n\n🛒 Оберіть товар:",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "КОБРА-1 МВС — 300", callback_data: "product_cobra_mvs" }],
+          [{ text: "КОБРА-1Н 100 мл — 250", callback_data: "product_cobra100" }],
+          [{ text: "ТЕРЕН-4 — 250", callback_data: "product_teren4" }],
+          [{ text: "ТЕРЕН-4М — 290", callback_data: "product_teren4m" }],
+          [{ text: "ТРИЗУБ-4 — 250", callback_data: "product_trizub4" }],
+          [{ text: "КОБРА-1Н 50 мл — 200", callback_data: "product_cobra50" }]
+        ]
+      }
+    }
   );
 
   return;
