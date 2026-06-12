df0de27333e90aa839b1031e46d304517a05e335
annrb <reichkol@icloud.com>
2026-06-13 00:30:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 const ADMIN_ID = 455696990;
 
 const SHEET_URL =
-  "https://script.google.com/macros/s/AKfycbx-QEsDJWrLN9QHxRfHdy6ombFp7QYMnF11A4_ARkG8ak4Oy8nFUUQLI5NZBkaLx3g/exec";
+"https://script.google.com/macros/s/AKfycbza3KxZFkbx0wqbUH53vwna_SO0slTDRb_0XFvN08djSBKDvnHdUm6TKYEdfbKprBDb/exec";
 
 const waitingReview = new Set();
 const waitingPaymentProof = new Set();
