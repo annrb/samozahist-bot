2701e9944e5c22d64b08514d9cc1149a66ba229b
annrb <reichkol@icloud.com>
2026-07-28 19:57:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1394,6 +1394,7 @@ await sendMessage(
           [{ text: "ТРИЗУБ-4 — 250", callback_data: "product_trizub4" }],
           [{ text: "КОБРА-1Н 50 мл — 200", callback_data: "product_cobra50" }]
 		  [{ text: "ТЕРЕН-1Б 50 мл — 220", callback_data: "product_teren1b" }]
+		  [{ text: "ТЕСТ", callback_data: "product_teren1b" }]
 		]
       }
     }
