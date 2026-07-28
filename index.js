f53d66997af27a182b2256ab209a49aca51d8cb6
annrb <reichkol@icloud.com>
2026-07-28 19:44:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -534,6 +534,7 @@ if (data === "clearcart") {
           [{ text: "ТЕРЕН-4М — 290", callback_data: "product_teren4m" }],
           [{ text: "ТРИЗУБ-4 — 250", callback_data: "product_trizub4" }],
           [{ text: "КОБРА-1Н 50 мл — 200", callback_data: "product_cobra50" }]
+		  [{ text: "ТЕРЕН-1Б 50 мл — 220", callback_data: "product_teren1b" }],
         ]
       }
     }
